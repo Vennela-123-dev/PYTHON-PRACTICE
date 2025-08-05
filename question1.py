b15482d607a39769af26575d9c526e3d0bb0fd96
Vennela-123-dev <vennelakata504@gmail.com>
2025-08-05 23:36:30 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -8,8 +8,9 @@ current_directory=os.getcwd()
 print("current directory:",current_directory)
 
 """"
-Naresh@DESKTOP-MGP9AJ0 MINGW64 ~/OneDrive/PYTHONSCRIPT
+output:
+Naresh@DESKTOP-MGP9AJ0 MINGW64 ~/OneDrive/PYTHONSCRIPT/PYTHON-PRACTICE (main)
 $ python question1.py
-current directory: C:\Users\Naresh\OneDrive\PYTHONSCRIPT
+current directory: C:\Users\Naresh\OneDrive\PYTHONSCRIPT\PYTHON-PRACTICE
 """"
 
